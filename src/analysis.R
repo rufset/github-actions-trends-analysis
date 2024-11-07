f887154296e66eb25e9f4c8384743cbebf620076
Linda Erlenhov <linda.erlenhov@gmail.com>
2024-11-07 22:04:30 +0100

    add trigger scripts

--- src/analysis.R
+++ src/analysis.R
@@ -5,6 +5,7 @@ library(effsize)
 my_palette <- c("#eae4e9","#fff1e6","#fde2e4","#fad2e1","#e2ece9","#bee1e6","#f0efeb","#dfe7fd","#cddafd")
 my_palette <- c("#ffadad","#ffd6a5","#fdffb6","#caffbf","#9bf6ff","#a0c4ff","#bdb2ff","#ffc6ff","#fde2e4")
 my_palette <- c("#303638","#f0c808","#5d4b20","#469374","#9341b3","#e3427d","#e68653","#ebe0b0","#edfbba")
+my_palette <- c("#EBD9B2","#D9B466","#aed9d6","#5BB4AC","#9A609A","#5B507A","#74A1CF","#083D77","#777777")
 
 del_outliers_iqr <- function(data_df, column) {
   filtered_df <- data_df %>% 
@@ -29,14 +30,14 @@ languages_df <- raw_data_df %>%
          PercAdoption = round(100*WithWorkflow/Count,1))
 
 lang_plot <- ggplot(languages_df, aes(x = reorder(mainLanguage, -PercAdoption), y = PercAdoption, label = paste(PercProjs,"%"))) +
-  geom_col(width = 0.8, fill = "#bcb6f6") +
+  geom_col(width = 0.8, fill = "#5BB4AC") +
   geom_hline(yintercept = 50, linetype = "dashed") +
   geom_text(aes(y = 5), vjust = -0.5, size = 3) +
   scale_y_continuous(breaks = seq(0,100,by=20), limits = c(0,100)) +
   labs(y = "Perc. of GA Adoption", x = "Programming Languages") +
   theme_bw() + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), legend.position = "none")
 
-#ggsave(lang_plot, filename = "figs/LanguagePlot.pdf", device = cairo_pdf(), width = 25, height = 12, units = "cm")
+ggsave(lang_plot, filename = "figs/LanguagePlot2.pdf", device = cairo_pdf(), width = 25, height = 12, units = "cm")
 
 ## Stargazers
 
@@ -64,7 +65,7 @@ stars_plot <- ggplot(summarised_stars_df, aes(x = reorder(Stars, MaxStars), y = 
   labs(y = "Perc. of GA Adoption", x = "Num. of Stars Per Sub-group") +
   theme_bw() + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), legend.position = "none")
 
-#ggsave(stars_plot, filename = "figs/StarsPlot.pdf",device = cairo_pdf(), width = 25, height = 12, units = "cm")
+ggsave(stars_plot, filename = "figs/StarsPlot2.pdf",device = cairo_pdf(), width = 25, height = 12, units = "cm")
 
 ## Contributors  
 
@@ -89,7 +90,7 @@ contrib_plot <- ggplot(summarised_contrib_df, aes(x = MaxContrib, y = PercAdopti
   labs(y = "Perc. of GA Adoption", x = "Num. of Contributors Per Sub-group") +
   theme_bw() + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), legend.position = "none")
 
-#ggsave(contrib_plot, filename = "figs/ContributorPlot.pdf",device = cairo_pdf(), width = 25, height = 12, units = "cm")
+ggsave(contrib_plot, filename = "figs/ContributorPlot2.pdf",device = cairo_pdf(), width = 25, height = 12, units = "cm")
 
 
 # Comparison of projects_
